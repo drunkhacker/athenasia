5d0a3cfc7c3efdbf298382552df3c9a3a4350a56
Jaehwa Han <drh@snu.ac.kr>
2012-11-09 20:39:23 -0600

    require child process

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@
 
 exports.index = function(req, res){
 	//grab the commit number
-	exec('git rev-parse HEAD', {cwd: __dirname}, function(error, stdout, stderr) {
+	require('child_process').exec('git rev-parse HEAD', {cwd: __dirname}, function(error, stdout, stderr) {
 		console.log('stdout : ' + stdout);
 		var commitid = stdout.replace(/\n$/, "");
 		res.render('index', { title: 'Athenasia', commit:commitid });
