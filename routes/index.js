692edbdc4581993b3605355f3df6a56ed2016b32
Jaehwa Han <drh@snu.ac.kr>
2012-11-09 23:09:33 -0600

    remove debug thing

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,7 @@
  */
 
 exports.index = function(lastDate, lastCommit) {
-    console.log(lastDate);
-    console.log(lastCommit);
-
 	return function(req, res){
 		res.render('index', { title: 'Athenasia', commit:lastCommit, date:lastDate });
-	    };
+	};
 }
