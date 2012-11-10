3f2d62649483800cd519efdf1b80b9372f19c25d
Jaehwa Han <drh@snu.ac.kr>
2012-11-09 21:03:35 -0600

    add some debug console output

--- routes/git.js
+++ routes/git.js
@@ -3,7 +3,7 @@ exports.pushed = function(req, res) {
 	var pushInformation = req.param;
 	var ip = req.ip;
     
-    
+
 	var authorized_ips = ["207.97.227.253", "50.57.128.197", "108.171.174.178"];
 
     if (authorized_ips.indexOf(ip) < 0) { //not authorized
@@ -20,6 +20,7 @@ exports.pushed = function(req, res) {
 
     		if (commitid == pushInformation.before) {
     			//fine to git pull
+                console.log("let's git pull");
     			exec("git reset --hard; git pull; sudo PORT=80 forever restart app.js", {cwd:__dirname}, function(error, stdout, stderr) {
     				if (error) {
     					console.log("Error : " + error);
@@ -27,6 +28,7 @@ exports.pushed = function(req, res) {
     			})
 
     		} else {
+                console.log("commit id didn't match " + commitid + ", " + pushInformation.before);
     			//what??
     		}
     	});
